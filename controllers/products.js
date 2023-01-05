458b19eaec339474749d6335fc700824c85bdf6b
webfaisalbd <web.faisal.bd@gmail.com>
2023-01-05 17:01:19 +0600

    pagination added

--- controllers/products.js
+++ controllers/products.js
@@ -11,7 +11,7 @@ const getAllProductsTest = async (req, res) => {
     const { company, name, sort, select } = req.query;
 
     const queryObject = {};
-    
+
     if (company) {
         queryObject.company = company;
         // console.log(queryObject);
@@ -23,18 +23,24 @@ const getAllProductsTest = async (req, res) => {
 
     let apiData = Product.find(queryObject).sort(queryObject);
 
-    if(sort){
+    if (sort) {
         // let sortFix = sort.replace("," , " ");
         let sortFix = sort.split(",").join(" ");
         apiData = apiData.sort(sortFix);
     }
 
-    if(select){
+    if (select) {
         // let selectFix = select.replace("," , " ");
         let selectFix = select.split(",").join(" ");
         apiData = apiData.select(selectFix);
     }
 
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+
+    let skip = (page - 1) * limit;
+
+    apiData = apiData.skip(skip);
 
     const myData = await apiData;
     res.status(200).json({ myData });
